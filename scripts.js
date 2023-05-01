364bee5557a298f131627af2b5403cfc6d902901
jony0418 <78985884+jony0418@users.noreply.github.com>
2023-05-01 08:06:56 -0600

    modify the script

--- scripts.js
+++ scripts.js
@@ -14,12 +14,86 @@ const mapDiv = document.getElementById('map');
 let map;
 function initMap() {
     map = new google.maps.Map(mapDiv, {
-        center: { lat: 32.796574, lng: -96.778978 },
-        zoom: 15,
+        center: { lat: 37.7749, lng: -122.4194 },
+        zoom: 10,
     });
 }
-initMap();
 
+// Search function
+searchButton.addEventListener('click', async () => {
+    const location = locationSearch.value;
+    const min = minPrice.value;
+    const max = maxPrice.value;
 
+    if (!location || !min || !max) {
+        alert('Please fill in all search fields.');
+        return;
+    }
+
+    // Fetch data from SimplyRETS API and filter by price range
+    const properties = await fetchProperties(location, min, max);
+
+    // Display properties on Google Maps
+    displayPropertiesOnMap(properties);
+});
+
+// Fetch data from SimplyRETS API and filter by price range (function definition)
+async function fetchProperties(location, min, max) {
+    const url = new URL('https://api.simplyrets.com/properties');
+    const params = {
+        q: location,
+        minprice: min,
+        maxprice: max,
+    };
+
+    url.search = new URLSearchParams(params);
+
+    const response = await fetch(url, {
+        headers: {
+            'Authorization': 'Basic ' + btoa(SIMPLYRETS_API_KEY + ':' + SIMPLYRETS_API_SECRET),
+        },
+    });
+
+    if (!response.ok) {
+        throw new Error(`Error fetching properties: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+    return data;
+}
+
+// Display properties on Google Maps (function definition)
+function displayPropertiesOnMap(properties) {
+    // Clear any existing markers
+    clearMarkers();
+
+    // Loop through properties and add markers to the map
+    properties.forEach((property) => {
+        const marker = new google.maps.Marker({
+            position: {
+                lat: property.geo.lat,
+                lng: property.geo.lng,
+            },
+            map,
+        });
+
+        // Add an info window to display property details when the marker is clicked
+        const infoWindow = new google.maps.InfoWindow({
+            content: `
+                <h3>${property.address.full}</h3>
+                <p>Price: $${property.listPrice}</p>
+                <p>Bedrooms: ${property.property.bedrooms}</p>
+                <p>Bathrooms: ${property.property.bathsFull}</p>
+            `,
+        });
+
+        marker.addListener('click', () => {
+            infoWindow.open(map, marker);
+        });
+    });
+}
 
 // Clear markers from the map (function definition)
+function clearMarkers() {
+    // Implement a function to clear any existing markers on the map
+}
